e8642aedbe3264a711d1a6cc7b5e7c365cc2f546
Matthew Pham <matthewpham55@gmail.com>
2019-09-07 14:17:48 -0500

    Initial java starter pack

---
+++ src/mech/mania/game_API.java
@@ -0,0 +1,25 @@
+package mech.mania;
+
+public class game_API {
+	Game game;
+	String playerName;
+	public game_API(Game g, String playerName){
+		this.game = g;
+		this.playerName = playerName;
+	}
+	public Unit[] getMyUnits() {		
+		return null;
+	}
+	
+	public Unit[] getEnemyUnits() {
+		return null;
+	}
+	
+	public Unit getUnitAt(Position pos) {
+		return null;
+	}
+	
+	public Tile getTile(Position pos) {
+		return null;
+	}
+}
